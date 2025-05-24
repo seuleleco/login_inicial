3df7b9fa26e1d2e8da378451d12af8f04070f7b1
ILHA SERVICE - Leonardo Pereira Pereira <leonardo.pereira.3@ebserh.gov.br>
2025-05-23 21:13:43 -0300

    working at login function and database

--- js/login.js
+++ js/login.js
@@ -7,12 +7,6 @@ function fazerLogin() {
     const usuarioDigitado = document.getElementById("usertext").value;
     const senhaDigitada = document.getElementById("password").value;
 
-    
-
-    
-    
-    
-    
   
     const usuarioEncontrado = dadosSalvo.find((usuario) => {
         return usuario.user === usuarioDigitado && usuario.senha === senhaDigitada;
@@ -31,9 +25,16 @@ function fazerLogin() {
         window.location.href = "redirects/aluno_area.html"; 
        alert("Login realizado com sucesso!");
   }
-    
+    else if (usuarioEncontrado.usertype === "professor"){
+        window.location.href = "redirects/prof_area.html";
+        alert("Login realizado com sucesso!");
+    }
+    else if (usuarioEncontrado.usertype === "diretor"){
+        window.location.href = "redirects/direc_area.html";
+        alert("Login realizado com sucesso!");
+    }
      else {
-        alert("Usuário ou senha incorretos!");
+        alert("/index.html");
     
     }
 
